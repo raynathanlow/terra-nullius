e849aa1aee5cf87eed89fa9ed62eea9086a15693
Ray Nathan Low <raynathanlow@gmail.com>
2018-08-17 23:42:18 -0700

    Add new scene with basic environment and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public Rigidbody theRB;
+    public float jumpForce;
+
+	// Use this for initialization
+	void Start () {
+        theRB = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }
+	}
+}
