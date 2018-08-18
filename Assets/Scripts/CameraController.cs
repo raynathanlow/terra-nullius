9d465832409fbf1ef7120bdbbb95dde713a7e068
Ray Nathan Low <raynathanlow@gmail.com>
2018-08-18 00:08:09 -0700

    Improve camera based on character position and smoothen jumping

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValues;
+    public float rotateSpeed;
+    public Transform pivot;
+	// Use this for initialization
+	void Start () {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        // Get the X position of the mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        // Get the Y position of the mouse and rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        // Move the camera based on the current rotation of the target and the original offset
+        float desiredXAngle = pivot.eulerAngles.x;
+        float desiredYAngle = target.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        // transform.position = target.position - offset;
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - 0.5f, transform.position.z);
+        }
+
+        transform.LookAt(target);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float moveSpeed;
     // public Rigidbody theRB;
@@ -12,14 +13,16 @@ public class PlayerController : MonoBehaviour {
     private Vector3 moveDirection;
     public float gravityScale;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         // theRB = GetComponent<Rigidbody>();	
         controller = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         // theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
 
         //if (Input.GetButtonDown("Jump"))
@@ -27,14 +30,22 @@ public class PlayerController : MonoBehaviour {
         //    theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
         //}
 
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+        // moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
 
-        if (Input.GetButtonDown("Jump"))
+        if (controller.isGrounded)
         {
-            moveDirection.y = jumpForce;
+            moveDirection.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
         }
 
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
         controller.Move(moveDirection * Time.deltaTime);
-	}
+    }
 }
