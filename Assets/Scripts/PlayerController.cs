1c8d0561df5f28f81153782d5aca2a1bbfa75dbe
Ray Nathan Low <raynathanlow@gmail.com>
2018-08-17 23:54:08 -0700

    Add better movement to character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
-    public Rigidbody theRB;
+    // public Rigidbody theRB;
     public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
 
 	// Use this for initialization
 	void Start () {
-        theRB = GetComponent<Rigidbody>();	
+        // theRB = GetComponent<Rigidbody>();	
+        controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        // theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        //}
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
 
         if (Input.GetButtonDown("Jump"))
         {
-            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+            moveDirection.y = jumpForce;
         }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
 	}
 }
