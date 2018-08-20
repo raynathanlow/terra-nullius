2eeb3e6977d6d0db7fd162c4532992807b8e5898
Ray Nathan Low <raynathanlow@gmail.com>
2018-08-20 16:08:15 -0700

    Add basic fire attack that ignites certain objects

---
+++ Assets/Scripts/Ignite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ignite : MonoBehaviour {
+
+    private GameObject flame;
+
+    private void Start()
+    {
+        flame = this.transform.Find("Flame").gameObject;
+        //flame.SetActive(true);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Fire Attack")
+        {
+            flame.SetActive(true);
+        }
+    }
+}
